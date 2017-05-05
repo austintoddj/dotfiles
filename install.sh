14fd68ce2db006edc00da9d09a0718987d76aa9e
Todd Austin <austin.todd.j@gmail.com>
2017-05-05 09:55:05 -0500

    Refactor base commands into base.sh

--- install.sh
+++ install.sh
@@ -16,15 +16,8 @@ echo "------------------------------------------------"
 echo -e $COL_YELLOW"Setting up your macOS development environment..."$COL_RESET
 echo "------------------------------------------------"
 
-# Update the base elements
-cp -r .vim ~/.vim
-cp -rf .vimrc ~/.vimrc
-cp -rf .bashrc ~/.bashrc
-cp -rf .gitconfig ~/.gitconfig
-cp -rf .gitignore ~/.gitignore
-cp -rf .hushlogin ~/.hushlogin
-cp -rf .bash_profile ~/.bash_profile
-cp -rf .git-prompt.sh ~/.git-prompt.sh
+# Include the base elements script
+./base.sh
 
 ###############################################################################
 # General UI/UX                                                               #
