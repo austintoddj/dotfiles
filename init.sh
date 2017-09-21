135da310a9f80c0e559c5cc84c782717d0c7580a
Todd Austin <austin.todd.j@gmail.com>
2017-09-21 10:00:25 -0500

    Replace the dock display because it looks cleaner

--- init.sh
+++ init.sh
@@ -224,8 +224,6 @@ defaults write com.apple.dock dashboard-in-overlay -bool true
 
 # Remove the auto-hiding Dock delay
 defaults write com.apple.dock autohide-delay -float 0
-# Remove the animation when hiding/showing the Dock
-defaults write com.apple.dock autohide-time-modifier -float 0
 
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
